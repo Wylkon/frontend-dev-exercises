a2d4aee342abc2bb4f26f4b8d3dce2590300522f
Wylkon Queiroz <wylkon@wylkon.com.br>
2015-02-15 21:28:12 -0200

    Improved logical search, implemented cache with local storage, show list of repositories and show a unique repository

--- Github Getter/js/app.js
+++ Github Getter/js/app.js
@@ -12,6 +12,8 @@ githubSearchRepo = (function($) {
 
   function bindInput() {
     var $input = $("#search");
+
+    // Limit Requisitions
     var searchDelay = (function(){
       var timer = 0;
       return function(callback, ms){
@@ -20,46 +22,101 @@ githubSearchRepo = (function($) {
       };
     })();
 
+    // Set Focus to input
     $input.focus();
-    $input.on('keyup', function(e) {
 
+    // When keyup, initiate search
+    $input.on('keypress', function(e) {
       searchDelay(function(){
+
         // Set search string
         var searchString = $input.val();
-        // if(searchString.length >= 3){
-        //   searchRepo(searchString);
-        // };
-        // return false;
-        console.log(searchString)
+
+        // The search string must be at least 3 characters
+        if(searchString.length >= 3){
+          searchRepo(searchString);
+        };
+        return false;
       }, 1000 );
     });
   }
 
   function searchRepo(searchString) {
-    var request = $.ajax({
-      url: "https://api.github.com/search/repositories?q="+searchString,
-      // url: "http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json",
-      cache: true,
-      dataType: "jsonp",
-      ifModified: true
+    var $containerResults = $("#results-container");
+    var actualSearch      = JSON.parse(localStorage.getItem(searchString));
+
+    if(actualSearch) {
+      removeRepositoryList();
+      implementRepositoryList(actualSearch['repositories']);
+    } else {
+      var request = $.ajax({
+        url: "https://api.github.com/legacy/repos/search/"+searchString,
+        dataType: "json",
+        beforeSend: function () {
+          $containerResults.addClass('gg-loading');
+          removeRepositoryList();
+        }
+      });
+
+      request.done( function(result) {
+        // Put the object into storage
+        localStorage.setItem(searchString, JSON.stringify(result));
+
+        $containerResults.removeClass('gg-loading');
+        implementRepositoryList(result['repositories']);
+      });
+    }
+  }
+
+  function removeRepositoryList(){
+    var $containerResults = $("#results-container ul").html("");
+  }
+
+  function implementRepositoryList(result) {
+    var $containerResults = $("#results-container");
+    $containerResults.addClass('gg-has-result')
+
+    $.each(result, function() {
+      var repositoryTemplate = "<li><a href='#'>"+this.name+" <span>by: <strong>"+this.owner+"</strong><span></a></li>"
+      $("ul", $containerResults).append(repositoryTemplate);
     });
 
-    request.done( function(data) {
-      console.log('teste')
-      updateTotalCount(data.total_count);
+    bindListRepository(result);
+  }
+
+  function bindListRepository(result) {
+    var $containerResults = $("#results-container ul");
+    $("#results-container li a").off('click');
+    $("li a",$containerResults).on('click', function (e) {
+      e.preventDefault();
+      var parentIndex = $(this).parent("li").index();
+      implementRepositoryView(result[parentIndex]);
     });
   }
 
-  function updateTotalCount(totalCount) {
-    var $containerResults = $("#results-container");
+  function implementRepositoryView(repository) {
+    var modalTemplate = "<a href='#' class='gg-modal-close'>close</a><h1>"+repository['name']+" <small>Language: "+repository['language']+" / Followers: "+repository['followers']+"</small></h1>"
+                      + "<div class='gg-description'>"+repository['description']+"</div>"
+                      + "<label data-prefix='URL:'><input type='text' value='"+repository['url']+"' readonly></label>"
 
-    totalCount = totalCount > 1 ? totalCount+" results" : totalCount+" result";
+    $(".gg-modal").html(modalTemplate).removeClass("gg-modal-closed");
+    bindClose();
+  }
 
-    if($containerResults.hasClass('gg-has-result')) {
-      $containerResults.find("p").html("<p>Your search found <strong>"+totalCount+"</strong>.</p>");
-    } else {
-      $containerResults.addClass('gg-has-result').append("<p>Your search found <strong>"+totalCount+"</strong>.</p>");
-    }
+  function bindClose() {
+    $(".gg-modal-close").off('click');
+    $(document).off('keyup');
+
+    $(".gg-modal-close").on('click', function (e) {
+      e.preventDefault();
+      $(this).parent('.gg-modal').addClass("gg-modal-closed");
+    });
+
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) { // esc keycode
+        $(".gg-modal-close").parent('.gg-modal').addClass("gg-modal-closed");
+      }
+    });
   }
 
   return {
